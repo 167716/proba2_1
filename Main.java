a5def8d80176eab089ec0a91ad27e228a450b90d
167716 <101186863+167716@users.noreply.github.com>
2022-04-01 18:35:49 +0000

    1

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner myObj=new Scanner(System.in);
+    
+    System.out.println("Podaj tekst do zamiany");
+    
+    String tekst=myObj.nextLine();
+    int rozmiar=tekst.length();
+    
+    char zamiana[]=new char[rozmiar];
+
+    for(int i=0;i<rozmiar;i++){
+      zamiana[i]=tekst.charAt(i);
+    }
+     for(int i=0;i<rozmiar;i++){
+      if()
+    }
+   /* char a='p';
+    int b=(int)a;
+    System.out.println(a+" "+b);
+    b=b-32;
+    a=(char)b;
+    System.out.println(a+" "+b);*/
+    
   }
 }
